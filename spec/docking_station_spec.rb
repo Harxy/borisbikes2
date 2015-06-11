f9d20b482dd01fc5c68075293a7f834649d5dd8e
Michael Arnott <arnottmj@aol.com>
2015-06-11 11:58:58 +0100

    added more methods

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,18 +2,23 @@ require 'docking_station'
 
 describe DockingStation do
   it "release_bike should fail when no bike is available" do
-    expect{ subject.release_bike }.to raise_error "No bikes available"
+    expect {subject.release_bike }.to raise_error "No bikes available"
 
   end
 
   it "release_bike release bike when bike is available" do
-    station = subject.dock(Bike.new)
-    expect(station.release_bike).to be_working
+    subject.dock(Bike.new)
+    expect(subject.release_bike).to be_working
 
   end
 
-  it {expect(subject.release_bike).to be_working}
-
   it {expect(subject).to respond_to(:dock).with(1).argument}
+
+    it "release_bike release bike when bike is available" do
+    subject.dock(Bike.new)
+    subject.release_bike
+    expect {subject.release_bike }.to raise_error "No bikes available"
+
+  end
 
 end
